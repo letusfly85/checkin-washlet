9f7e2ab78a5c15eaf53208f3f0e1a89af9c32f46
letusfly85 <letusfly95@gmail.com>
2016-04-04 16:22:15 +0900

    LTMorphingLabel

--- checkin.washlet/NextViewController.swift
+++ checkin.washlet/NextViewController.swift
@@ -1,8 +1,26 @@
 import UIKit
+import LTMorphingLabel
 
-class NextViewController: UIViewController {
+class NextViewController: UIViewController, LTMorphingLabelDelegate {
 
-    @IBOutlet weak var washletDetail: UILabel!
+    var textArray = [
+        "tripleW",
+        "DFree",
+        "Go Go!"
+    ]
+    
+    var i = 0
+    
+    var text:String {
+        get {
+            if i >= textArray.count {
+                i = 0
+            }
+            return textArray[i++]
+        }
+    }
+
+    @IBOutlet weak var washletDetail: LTMorphingLabel!
     
     var appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     
@@ -11,10 +29,15 @@ class NextViewController: UIViewController {
         
         let num = String(appDelegate.washletId!)
         washletDetail.text = String(appDelegate.washletsMap.valueForKey(num)!)
+        
+    }
+    @IBAction func changeText(sender: AnyObject) {
+        washletDetail.text = text
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 }
